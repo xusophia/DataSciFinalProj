b4b180467eb4cad2a38961a16f0a6357823e5fd2
indhu-gunda <indhusri.gunda@gmail.com>
2020-12-01 20:51:07 -0600

    compatible with windows & add gitignore

--- VanillaDqn.py
+++ VanillaDqn.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.nn import Sequential
 from collections import deque
 import numpy as np
+
 '''
 Param defs:
 env: gym env
@@ -13,13 +14,15 @@ epsilon: exploration rate
 epsilon_decay: decay rate for exploration rate after each episode
 buffer_size: for experience replay...storing past information on states, actions, rewards, next_states, done
 '''
+
+
 class DQN(nn.Module):
-    def __init__(self, env, lr=1e-4, gamma = 0.99, epsilon=1.0, epsilon_decay=0.995, buffer_size=500000):
+    def __init__(self, env, lr=1e-4, gamma=0.99, epsilon=1.0, epsilon_decay=0.995, buffer_size=500000):
         super(DQN, self).__init__()
-        #input output dimensions
+        # input output dimensions
         self.state_space_dim = env.observation_space.shape[0]
         self.action_space_dim = env.action_space.n
-        #our state-action function estimator
+        # our state-action function estimator
         self.model = Sequential(
             nn.Linear(self.state_space_dim, 512),
             nn.ReLU(),
@@ -33,14 +36,14 @@ class DQN(nn.Module):
         self.epsilon_decay = epsilon_decay
         self.env = env
         self.buffer = deque(maxlen=buffer_size)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)  #updates the weights of the model after computing gradients
-        self.loss_fn = nn.MSELoss()     #defining our loss function to be the MSE loss
-
+        self.optimizer = torch.optim.Adam(self.model.parameters(),
+                                          lr=self.lr)  # updates the weights of the model after computing gradients
+        self.loss_fn = nn.MSELoss()  # defining our loss function to be the MSE loss
 
     def insert(self, state, action, reward, next_state, done):
         self.buffer.append((state, action, reward, next_state, done))
 
-    #Get a mini-batch to train the model
+    # Get a mini-batch to train the model
     def sample_buffer(self, num_samples):
         states, actions, rewards, next_states, dones = [], [], [], [], []
         idx = np.random.choice(len(self.buffer), num_samples)
@@ -53,7 +56,7 @@ class DQN(nn.Module):
             next_states.append(np.array(next_state, copy=False))
             dones.append(done)
         states = torch.as_tensor(np.array(states))
-        actions = torch.as_tensor(np.array(actions))
+        actions = torch.as_tensor(np.array(actions, dtype=np.int64))
         rewards = torch.as_tensor(np.array(rewards, dtype=np.float32))
         next_states = torch.as_tensor(np.array(next_states))
         dones = torch.as_tensor(np.array(dones, dtype=np.float32))
@@ -67,12 +70,12 @@ class DQN(nn.Module):
 
     def train_batch(self, states, actions, rewards, next_states, dones):
         # Bellman equation for updates
-        targets = rewards + self.gamma * self.model(next_states).max(-1).values * (1.0-dones)
+        targets = rewards + self.gamma * self.model(next_states).max(-1).values * (1.0 - dones)
         preds = self.model(states)
         action_masks = F.one_hot(actions, self.action_space_dim)
         preds = (preds * action_masks).sum(dim=-1)
         loss = self.loss_fn(preds, targets.detach())
-        self.optimizer.zero_grad() #zero out the gradients for weights of model
-        loss.backward() #compute the gradient of loss with respect to model paramenters
+        self.optimizer.zero_grad()  # zero out the gradients for weights of model
+        loss.backward()  # compute the gradient of loss with respect to model paramenters
         self.optimizer.step()
         return loss
--- lunarlander1.py
+++ lunarlander1.py
@@ -2,6 +2,7 @@ import gym
 from VanillaDqn import DQN
 import numpy as np
 import torch
+
 env = gym.make('LunarLander-v2')
 episodes = 1000
 epsilon = 1.0
@@ -11,15 +12,17 @@ counter = 0
 update_t = 5
 batch_size = 32
 lr = 1e-4
-network = DQN(env, lr = lr, gamma = gamma, epsilon = epsilon, buffer_size =buffer_size)
+network = DQN(env, lr=lr, gamma=gamma, epsilon=epsilon, buffer_size=buffer_size)
 reward_list_ep = []
 
-for episode in range(episodes+1):
+print(torch.__version__)
+
+for episode in range(episodes + 1):
     state = env.reset().astype(np.float32)
     reward_ep, done = 0, False
     reward_last_100_eps = []
     while not done:
-        env.render() # Comment this if you do not want rendering
+        env.render()  # Comment this if you do not want rendering
         state_tensor = torch.from_numpy(state)
         action = network.get_action(state_tensor)
         next_state, reward, done, info = env.step(action)
@@ -31,15 +34,15 @@ for episode in range(episodes+1):
         counter += 1
         counter %= update_t
 
-        if len(network.buffer) > batch_size and counter==0: #update weights every 5 steps
+        if len(network.buffer) > batch_size and counter == 0:  # update weights every 5 steps
             states, actions, rewards, next_states, dones = network.sample_buffer(batch_size)
             loss = network.train_batch(states, actions, rewards, next_states, dones)
 
     if episode < 900:
-        network.epsilon*=network.epsilon_decay
+        network.epsilon *= network.epsilon_decay
     reward_list_ep.append(reward_ep)
 
-    if len(reward_last_100_eps)==100:
+    if len(reward_last_100_eps) == 100:
         reward_last_100_eps = reward_last_100_eps[1:0]
     reward_last_100_eps.append(reward_ep)
 
